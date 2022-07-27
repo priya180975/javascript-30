fc9f494eaef42ac6669939572e17e1951ad1d301
Priya <priyannayak100@gmail.com>
2022-07-27 10:34:52 +0530

    added audio

--- 01- javascript drum kit/script.js
+++ 01- javascript drum kit/script.js
@@ -1,15 +1,16 @@
-window.addEventListener("keydown",playsound,false)
+window.addEventListener("keydown",playsound)
 
 function playsound(e)
 {
-    console.log(e.keyCode)
-    // a=65 s=83 d=68 f=70 g=71 h=72 j=74 k=75 l=76
-
     if([65,83,68,70,71,72,74,75,76].includes(e.keyCode))
     {
         let selectedkey=document.querySelector(`[data-key="${e.keyCode}"]`)
         selectedkey.classList.add("animate")
         setTimeout(function(){selectedkey.classList.remove("animate")},200)
+
+        let type=selectedkey.children[1].innerHTML;
+
+        var audio=new Audio(`../sounds/${type}.wav`)
+        audio.play()
     }
-    
 }
