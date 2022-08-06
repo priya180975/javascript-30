ace76938138f7cb4cc38577a063048ab43df5ab6
Priya <priyannayak100@gmail.com>
2022-08-06 14:46:54 +0530

    play sound on click

--- 01- javascript drum kit/script.js
+++ 01- javascript drum kit/script.js
@@ -1,16 +1,19 @@
 window.addEventListener("keydown",playsound)
+let keys=document.querySelectorAll(".keys")
+keys.forEach(k=>k.addEventListener("click",function(){playsound(k)}))
 
 function playsound(e)
 {
-    if([65,83,68,70,71,72,74,75,76].includes(e.keyCode))
+    let keyCode=e.keyCode||parseInt(e.dataset.key)
+    if([65,83,68,70,71,72,74,75,76].includes(keyCode))
     {
-        let selectedkey=document.querySelector(`[data-key="${e.keyCode}"]`)
+        let selectedkey=document.querySelector(`[data-key="${keyCode}"]`)
         selectedkey.classList.add("animate")
         setTimeout(function(){selectedkey.classList.remove("animate")},200)
 
         let type=selectedkey.children[1].innerHTML;
 
-        var audio=new Audio(`../sounds/${type}.wav`)
+        var audio=new Audio(`sounds/${type}.wav`)
         audio.play()
     }
 }
